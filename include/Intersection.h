a2e649823efa74e9b1f2e0ebcf6d0a094e42aad8
Selçuk Öztürk <selcuk.ozturk@positive.com.tr>
2017-09-11 17:29:58 +0300

    Added intersection.

---
+++ include/Intersection.h
@@ -0,0 +1,15 @@
+//
+// Created by Selçuk Öztürk on 11/09/2017.
+//
+
+#ifndef ASSIGNMENT1_INTERSECTION_H
+#define ASSIGNMENT1_INTERSECTION_H
+
+#include "Rect.h"
+
+struct Intersection {
+    int rect1, rect2;
+    Rect intersectPoint;
+};
+
+#endif //ASSIGNMENT1_INTERSECTION_H
