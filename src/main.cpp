6270c73592e9efdb38b808d4d270daa16caca391
Selcuk Ozturk <selcukahmed@gmail.com>
2017-09-17 00:39:38 +0300

    Added a main exception handling.

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,23 @@
 int main(int argc, char** argv)
 {
     if (argc>1) {
-        RectangleOperator rectangleOperator;
-        rectangleOperator.load(argv[1]);
+        try {
+            RectangleOperator rectangleOperator;
+            rectangleOperator.load(argv[1]);
 
-        std::cout << OutputFormatter::rectsToString(rectangleOperator.getRects())
-                  << std::endl;
+            std::cout << OutputFormatter::rectsToString(rectangleOperator.getRects())
+                      << std::endl;
 
-        auto rectList = rectangleOperator.getIntersections();
+            auto rectList = rectangleOperator.getIntersections();
 
-        std::cout << OutputFormatter::rectListToString(rectList) << std::endl;
+            std::cout << OutputFormatter::rectListToString(rectList) << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
     else {
-        std::cout << "The program must have a json file path as parameter." << std::endl;
+        std::cerr << "The program must have a json file path as parameter." << std::endl;
     }
 
     return 0;
